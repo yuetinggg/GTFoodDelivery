9f0c266662687dd2bd224d38acb39d2a9b4093d3
ishaan <ishaangrover@gatech.edu>
2016-03-06 03:14:38 -0500

    First commit

---
+++ app/src/main/java/com/example/apple/gtdelivery/FoodChooserActivity.java
@@ -0,0 +1,13 @@
+package com.example.apple.gtdelivery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodChooserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_chooser);
+    }
+}
