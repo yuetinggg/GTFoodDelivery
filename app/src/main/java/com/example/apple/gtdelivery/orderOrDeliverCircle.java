fadaaf2a22505aa10d81105f7b86d3ee7357aaaf
Yueting <yuetingleeeee@gmail.com>
2016-03-06 12:52:08 -0500

    Added orderOrDeliver Screen and a Custom View
    -added orderOrDeliver Screen
    -Created a custom view for the orderOrDeliver screen

---
+++ app/src/main/java/com/example/apple/gtdelivery/orderOrDeliverCircle.java
@@ -0,0 +1,12 @@
+package com.example.apple.gtdelivery;
+
+import android.view.View;
+import android.util.AttributeSet;
+import android.content.Context;
+
+/**
+ * Created by yuetinggg on 3/6/16.
+ */
+public class orderOrDeliverCircle extends View {
+    
+}
