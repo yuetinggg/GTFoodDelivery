2db76edd9bd70ac6a7c8a6f6912950b464377590
Yueting <yuetingleeeee@gmail.com>
2016-03-06 19:23:41 -0500

    Update OrderOrDeliver Screen
    Added onTouchListener and Labels

--- app/src/main/java/com/example/apple/gtdelivery/OrderOrDeliver.java
+++ app/src/main/java/com/example/apple/gtdelivery/OrderOrDeliver.java
@@ -1,14 +1,49 @@
 package com.example.apple.gtdelivery;
 
+import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class OrderOrDeliver extends Activity {
+    orderOrDeliverCircle oodCircle;
+    RelativeLayout wholeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_or_deliver);
+        oodCircle = (orderOrDeliverCircle) findViewById(R.id.oodCircle);
+        wholeView = (RelativeLayout) findViewById(R.id.wholeView);
+        oodCircle.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Dividing the screen into sides
+                float touchX = event.getX();
+                float touchY = event.getY();
+
+                if (touchX < wholeView.getMeasuredWidth()/2) {
+                    toOrder();
+                } else {
+                    toDeliver();
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void toOrder() {
+        Intent i = new Intent(this, FoodChooserActivity.class);
+        startActivity(i);
+    }
+
+    private void toDeliver() {
+        Intent i = new Intent(this, FoodChooserActivity.class);
+        //startActivity(i);
     }
 
 }
--- app/src/main/java/com/example/apple/gtdelivery/orderOrDeliverCircle.java
+++ app/src/main/java/com/example/apple/gtdelivery/orderOrDeliverCircle.java
@@ -13,11 +13,11 @@ import android.content.Context;
  */
 public class orderOrDeliverCircle extends View {
     //Keeping tracking of color and the labels of the circle
-    private int oColor, dColor, oLabelColor, dLabelColor;
+    private int oColor, dColor, oLabelColor, dLabelColor, lineColor;
     private static String order ="ORDER";
     private static String deliver="DELIVER";
     // Paint objects for when the View is drawn
-    private Paint oPaint, dPaint;
+    private Paint oPaint, dPaint, linePaint;
 
     public orderOrDeliverCircle(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,6 +25,7 @@ public class orderOrDeliverCircle extends View {
         //Create Paint object for OnDraw()
         oPaint = new Paint();
         dPaint = new Paint();
+        linePaint = new Paint();
 
         //Getting the attribute values set in the XML
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.orderOrDeliverCircle, 0, 0);
@@ -34,6 +35,7 @@ public class orderOrDeliverCircle extends View {
             dColor = a.getInteger(R.styleable.orderOrDeliverCircle_dSideColor, 0);
             oLabelColor = a.getInteger(R.styleable.orderOrDeliverCircle_oSideLabelColor, 0);
             dLabelColor = a.getInteger(R.styleable.orderOrDeliverCircle_dSideLabelColor, 0);
+            lineColor = a.getInteger(R.styleable.orderOrDeliverCircle_lineColor, 0);
         } finally {
             a.recycle();
         }
@@ -51,13 +53,27 @@ public class orderOrDeliverCircle extends View {
         oPaint.setColor(oColor);
         dPaint.setStyle(Paint.Style.FILL);
         dPaint.setColor(dColor);
+        linePaint.setColor(lineColor);
+        linePaint.setStyle(Paint.Style.FILL);
+        linePaint.setStrokeWidth(5f);
 
         //Creating the oval for the drawArc to fill
         RectF oval = new RectF(viewWidthHalf - radius, viewHeightHalf - radius, viewWidthHalf + radius, viewHeightHalf + radius);
 
         //Drawing the circle using the canvas and paint
         canvas.drawArc(oval, -75, 180, true, dPaint);
-        canvas.drawArc(oval, 106, 180, true, oPaint);
+        canvas.drawArc(oval, 105, 180, true, oPaint);
+
+        //Drawing the line
+        canvas.drawLine(viewWidthHalf - (radius / 2), (float) (viewHeightHalf + Math.tan(Math.toRadians(75)) * (radius / 2)), radius / 2 + viewWidthHalf, (float) (viewHeightHalf - Math.tan(Math.toRadians(75)) * (radius / 2)), linePaint);
+
+        //Draw the text
+        linePaint.setColor(oLabelColor);
+        linePaint.setTextAlign(Paint.Align.CENTER);
+        linePaint.setTextSize(75);
+        canvas.drawText(order, viewWidthHalf - radius + (radius / 5 * 3), viewHeightHalf, linePaint);
+        linePaint.setColor(dLabelColor);
+        canvas.drawText(deliver, viewWidthHalf + (radius/5 * 3), viewHeightHalf, linePaint);
 
     }
 
