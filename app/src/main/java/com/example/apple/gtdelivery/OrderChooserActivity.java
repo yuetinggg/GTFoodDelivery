d53a5f2819a531cecd016d1a27d36899f3769717
Yueting <yuetingleeeee@gmail.com>
2016-04-16 22:08:31 -0400

    Added some Firebase logic

--- app/src/main/java/com/example/apple/gtdelivery/OrderChooserActivity.java
+++ app/src/main/java/com/example/apple/gtdelivery/OrderChooserActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 
 import com.firebase.client.Firebase;
+import com.firebase.client.Query;
 
 public class OrderChooserActivity extends Activity {
 
@@ -13,6 +14,14 @@ public class OrderChooserActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_chooser);
+
+        //Setting up Firebase context
+        Firebase.setAndroidContext(this);
+        firebaseref = new Firebase("https://gtfood.firebaseio.com/");
+        Firebase statusTableRef = firebaseref.child("status_table");
+
+        Query orderQuery = firebaseref.orderByChild("Status").equalTo("O");
+
     }
 
 }
